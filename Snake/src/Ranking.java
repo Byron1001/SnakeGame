c4cdd4c2fe3ba108a5a7fb06335f331aafa0c5d7
SoongMingLiang <soongmingliang647@gmail.com>
2022-12-04 22:25:52 +0800

    Add methods of insert score and reset history and a simple documentation

--- Snake/src/Ranking.java
+++ Snake/src/Ranking.java
@@ -1,12 +1,24 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * @author SoongMingLiang (soongmingliang647@gmail.com)
+ * @version 1.0.0
+ *
+ * This class is to provide the ranking system service to the SnakeGame.
+ * The methods that are available to other classes currently are
+ * getHistory(), insertScore(int new_score), and resetHistory()
+ * The main functionalities of these methods are return the game score history, insert a new score into the file, and
+ * reset the score history of the game respectively.
+ */
 public class Ranking {
     private static final File historyScoreFile = new File ("history.txt");
     private static ArrayList<String> scoreHistory = new ArrayList<>();
+    private static ArrayList<Integer> scoreHistory_write = new ArrayList<>();
 
-    public static void readHistory(){
+    private static void readHistory(){
         try(BufferedReader scoreReader = new BufferedReader(new FileReader(historyScoreFile))){
             String score = scoreReader.readLine();
             if(score == null){
@@ -42,15 +54,64 @@ public class Ranking {
         }
     }
 
+    private static void sortHistory(){
+        Collections.fill(scoreHistory_write, 0);
+        for(var i : scoreHistory){
+            scoreHistory_write.add(Integer.parseInt(i));
+        }
+        Collections.sort(scoreHistory_write);
+    }
+
     public static ArrayList<String> getHistory(){
         readHistory();
         return scoreHistory;
     }
 
-    public static void main(String args[]){
-        ArrayList<String> history = getHistory();
-        for(String s : scoreHistory){
-            System.out.println(s);
+    public static void insertScore(int new_score){
+        String newScore = String.valueOf(new_score);
+        scoreHistory.add(newScore);
+        sortHistory();
+        try(FileWriter scoreWriter = new FileWriter(historyScoreFile, false)){
+            for(var i : scoreHistory_write){
+                if(i == 0){
+                    continue;
+                }
+                else{
+                    scoreWriter.write(i + "\n");
+                }
+            }
         }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void resetHistory(){
+        try(FileWriter scoreWriter = new FileWriter(historyScoreFile, false)){
+            scoreWriter.write("");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        Collections.fill(scoreHistory, null);
+        Collections.fill(scoreHistory_write, 0);
+    }
+
+    public static void main(String args[]){
+        /*ArrayList<String> history = getHistory();
+        for(String s : history){
+            System.out.println(s);
+        }*/
+
+        /*insertScore(10);
+        insertScore(300);
+        insertScore(100);*/
+
+        /*ArrayList<String> history = getHistory();
+        for(String s : history){
+            System.out.println(s);
+        }*/
+
+        /*resetHistory();*/
     }
 }
